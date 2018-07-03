1545e174d74581cc337e43497f7f05985fa29e95
Albert Santiago <albertsantiagodelacruz@gmail.com>
2018-07-02 21:50:11 -0600

    Agregando practica de debugging (Solo falto el formulario)

---
+++ Albert/Debugging/src/main/java/Servidor.java
@@ -0,0 +1,47 @@
+import static spark.Spark.*;
+
+/**
+ * Problemas conocidos
+ * 1 - La aplicacion no corre. (Hay 2 razones para esto)
+ * 2 - De correr, parece que los HTML no funcionan bien.
+	Pruebe la url, /p2.html, la cual no deberia retornar 404
+ * 3 - Al enviar el formulario de GET /form, la aplicacion se detiene.
+ */
+
+public class Servidor {
+	public static void main(String[] args) {
+		staticFileLocation("/static");
+		port(1234);
+		get("/", (req, res) -> {
+			res.redirect("/form");
+			return null;
+		});
+		
+		get("/p2", (req, res) -> {
+			res.redirect("/p2.html");
+			return null;
+		});
+		
+		get("/form", (req, res) -> {
+			// Al darle click al boton de enviar, la aplicacion lanza unos errores.
+			return "<form action='/post' method='POST'>"
+					+ "<button type='submit'/> Enviar </button>"
+					+ "</form>";
+			
+		});
+		
+		// No logre hacer esto
+		
+		post("/form", (req, res) -> {
+			if (req.session(true).attribute("formulario").equals(null)) {
+				// Establecemos la variable formulario con un valor:
+				req.session(true).attribute("formulario", "Muy bien, resolvieron el punto 3");
+				res.redirect("/form");
+			}
+			
+			// Se ha de retornar `Muy bien, resolvieron el punto 3`
+			// La 2da vez que se cliquea el boton de GET /form
+			return req.session(true).attribute("formulario");
+		});
+    }
+}
